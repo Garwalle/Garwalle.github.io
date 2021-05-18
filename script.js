8d224cb259b6f8f429fd1ffd476723c79f8dc1f0
Garwalle <Martin.Jeanne@sts-sio-caen.info>
2021-05-18 13:22:07 +0200

    Fixed a bug on animation

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ window.onload = function () { scrollFunction() };
 function scrollFunction() {
 
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        $("#banner").css("height", "8%");
         $("#page").css("filter", "blur(0px)");
-        $("#bannerImgs").css("width", "0");
-        $("#bannerImgs").fadeOut(450).promise().done(function () {
+        $(".bannerImg").css("width", "0");
+        $("#banner").css("height", "8%");
+        $(".bannerImg").fadeOut().promise().done(function () {
             $("#blanckNB").css("width", "100%");
             $("#NBImg").css("width", "18%");
         });
@@ -18,8 +18,8 @@ function scrollFunction() {
         $("#blanckNB").css("width", "0").promise().done(function () {
             $("#banner").css("height", "45%");
             $("#page").css("filter", "blur(4px)");
-            $("#bannerImgs").fadeIn();
-            $("#bannerImgs").css("width", "70%");
+            $(".bannerImg").fadeIn();
+            $(".bannerImg").css("width", "70%");
         });
     }
 }
